89fbee753a7109168bb0fda3f0360bbf08f49c66
Nordin Ouled L'Kadi <10020942@student.hhs.nl>
2017-11-21 15:38:36 +0100

    Add project files.

---
+++ Opdracht2Week1deel2/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Opdracht2Week1deel2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+
+}
---
+++ Opdracht2Week1deel2/Question.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Opdracht2Week1deel2
+{
+    class Question
+    {
+
+        private String Text { get; set; }
+        private String Answer { get; set; }
+
+        public Question()
+        {
+            Text = "";
+            Answer = "";
+        }
+
+        public void SetQuestion(String text)
+        {
+            this.Text = text;
+
+        }
+
+        public void SetAnswer(String answer)
+        {
+            this.Answer = answer;
+        }
+
+        public Boolean CheckAnswer(String response)
+        {
+            return response.Equals(Answer);
+
+        }
+
+        public void Display()
+        {
+            Console.WriteLine(Text);
+        }
+
+
+    }
+}
